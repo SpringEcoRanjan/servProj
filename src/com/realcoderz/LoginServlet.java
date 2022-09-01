0d0f7ae510ddc2daf385afba9896098969e20b6c
aditya-ranjan-ar-ha <aditya.ranjan@netsmartz.net>
2022-09-01 17:08:25 +0530

    first commit

---
+++ src/com/realcoderz/LoginServlet.java
@@ -0,0 +1,47 @@
+package com.realcoderz;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		String userName=request.getParameter("uname");
+		String password=request.getParameter("pwd");
+		if(userName.equals("System") && password.equals("admin")){
+			out.write("<h3>Welcome "+ userName+", Your login successful</h3>");
+		}else{
+			out.write("Login failed");
+		}
+		
+	}
+
+}
